14a6f416420c9936a32b4eb7d74fac1d339ec752
RiccardoPrestigiacomo-99 <50378585+RiccardoPrestigiacomo-99@users.noreply.github.com>
2021-06-05 21:30:43 +0200

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -5,9 +5,9 @@ import torch.utils.model_zoo as model_zoo
 """
 Credits to @hshustc
 Taken from https://github.com/hshustc/CVPR19_Incremental_Learning/tree/master/cifar100-class-incremental
-"""
 
-"""
+
+
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,padding=1, bias=False)
